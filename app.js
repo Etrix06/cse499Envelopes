56d01c295b096c42acf1d0947b6f8c6d002adb1c
Etrix06 <alc18005@byui.edu>
2022-04-18 21:07:24 -0700

    installed NPM, Nodemon, and Express

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/envelopes', (req, res, next) => {
+    res.send('<form action="/envelope" method="POST"><input type="text" name="title"><button type="submit">Add Envelope</button></form>');
 });
 
-server.listen(3000);
+app.use('/envelope', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello From Express line 7.</h1>');
+});
+
+app.listen(3000);
 
 console.log('Listening on 3000');
