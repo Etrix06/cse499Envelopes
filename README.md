f912af911344bc63e9c2a88e50b33a7794c272ac
Etrix06 <alc18005@byui.edu>
2022-04-23 23:35:39 -0700

    adding my ejs views

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cse499Envelopes
 My repository for my CSE 499 Senior Project
+
+currently reviewing academind course
+Module- Working with Express.js  using express router
+
+also on #7 of ninja
--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//express app
 const app = express();
+
+//view engine
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/envelopes', (req, res, next) => {
+app.use('/add-envelope', (req, res, next) => {
     res.send('<form action="/envelope" method="POST"><input type="text" name="title"><button type="submit">Add Envelope</button></form>');
 });
 
-app.use('/envelope', (req, res, next) => {
+app.post('/envelope', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express line 7.</h1>');
+app.get('/', (req, res, next) => {
+    res.render('index');
 });
 
+app.get('/login', (req, res, next) => {
+    res.render('login');
+});
+
+app.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+app.get('/envelopes/add-envelope', (req, res, next) => {
+    res.render('add-envelope');
+});
+
+//404 Page
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+
+//Listen for requests
 app.listen(3000);
 
 console.log('Listening on 3000');
