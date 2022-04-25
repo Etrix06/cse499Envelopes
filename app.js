bc68018781a63908d73029c834f3a3e56c16f122
Etrix06 <alc18005@byui.edu>
2022-04-24 22:11:22 -0700

    added includes, testing dynamic envelopes

--- app.js
+++ app.js
@@ -9,39 +9,40 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-envelope', (req, res, next) => {
-    res.send('<form action="/envelope" method="POST"><input type="text" name="title"><button type="submit">Add Envelope</button></form>');
-});
-
 app.post('/envelope', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
 app.get('/', (req, res, next) => {
-    res.render('index');
+    const testEnvelopes = [
+        { category: 'Eating Out', budgeted: 600, remaining: 450 },
+        { category: 'Gas', budgeted: 150, remaining: 100 },
+        { category: 'Entertainment', budgeted: 200, remaining: 130 }
+    ];
+    res.render('index', { title: 'Home', envelopes: testEnvelopes });
 });
 
 app.get('/login', (req, res, next) => {
-    res.render('login');
+    res.render('login', { title: 'Login' });
 });
 
 app.get('/signup', (req, res, next) => {
-    res.render('signup');
+    res.render('signup', { title: 'Sign Up' });
 });
 
 app.get('/about', (req, res, next) => {
-    res.render('about');
+    res.render('about', { title: 'About Us' });
 });
 
 app.get('/envelopes/add-envelope', (req, res, next) => {
-    res.render('add-envelope');
+    res.render('add-envelope', { title: 'Add Envelope' });
 });
 
 //404 Page
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404 Page Not Found' });
 });
 
 
