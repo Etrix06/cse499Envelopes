b6f0b10fb14f26c6d4af627f37cb4177fa432eda
Etrix06 <alc18005@byui.edu>
2022-05-17 21:15:34 -0700

    new branch

--- models/User.js
+++ models/User.js
@@ -16,7 +16,6 @@ const userSchema = new Schema({
         type: String,
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters']
-
     }
 });
 /*
